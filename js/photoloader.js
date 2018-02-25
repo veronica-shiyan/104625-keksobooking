2caad4f57ea6e939067941880eadda091aa8a56e
Veronica Shiyan <GingerRonny@yandex.ru>
2018-02-25 17:47:02 +0200

    Drag&drop

--- js/photoloader.js
+++ js/photoloader.js
@@ -4,7 +4,8 @@
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
   var PHOTO_STYLE = {
     width: 60,
-    height: 80
+    height: 80,
+    border: '2px solid transparent'
   };
 
   var form = document.querySelector('.notice__form');
@@ -14,6 +15,8 @@
   var photoInput = form.querySelector('#images');
   var avatarDropZone = document.querySelector('.notice__photo .drop-zone');
   var photoDropZone = photoContainer.querySelector('.drop-zone');
+
+  var targetDropElement = null;
 
   var readFiles = function (file, previewImage) {
     var fileName = file.name.toLowerCase();
@@ -45,6 +48,7 @@
     var photoImage = document.createElement('img');
     photoImage.style.width = PHOTO_STYLE.width + 'px';
     photoImage.style.height = PHOTO_STYLE.height + 'px';
+    photoImage.style.border = PHOTO_STYLE.border;
     photoContainer.appendChild(photoImage);
     readFiles(photoFiles, photoImage);
   }
@@ -53,15 +57,24 @@
     Array.from(photoInput.files).forEach(function (photoFiles) {
       photoLoadHandler(photoFiles);
     });
+    var photoImages = photoContainer.querySelectorAll('img');
+    [].forEach.call(photoImages, function (photoImage) {
+      photoImage.addEventListener('dragstart', dragstartHandler);
+      photoImage.addEventListener('dragover', dragoverHandler);
+      photoImage.addEventListener('dragenter', dragenterHandler);
+      photoImage.addEventListener('dragleave', dragleaveHandler);
+      photoImage.addEventListener('drop', dropHandler);
+      photoImage.addEventListener('dragend', dragendHandler);
+    });
   });
 
-  function dragover(evt) {
+  function dragoverLoadHandler(evt) {
     evt.stopPropagation();
     evt.preventDefault();
   }
 
-  avatarDropZone.addEventListener('dragover', dragover);
-  photoDropZone.addEventListener('dragover', dragover);
+  avatarDropZone.addEventListener('dragover', dragoverLoadHandler);
+  photoDropZone.addEventListener('dragover', dragoverLoadHandler);
 
   avatarDropZone.addEventListener('drop', function (evt) {
     evt.preventDefault();
@@ -72,4 +85,47 @@
     evt.preventDefault();
     photoInput.files = evt.dataTransfer.files;
   });
+
+  function dragstartHandler(evt) {
+    evt.target.style.opacity = '0.4';
+  }
+
+  function dragoverHandler(evt) {
+    targetDropElement = evt.target;
+    if (evt.preventDefault) {
+      evt.preventDefault();
+    }
+    evt.dataTransfer.dropEffect = 'move';
+    return false;
+  }
+
+  function dragenterHandler(evt) {
+    evt.target.style.border = '2px dashed black';
+  }
+
+  function dragleaveHandler(evt) {
+    targetDropElement = null;
+    evt.target.style.border = PHOTO_STYLE.border;
+  }
+
+  function dropHandler(evt) {
+    if (evt.stopPropagation) {
+      evt.stopPropagation();
+    }
+    return false;
+  }
+
+  function dragendHandler(evt) {
+    var url = '';
+    if (targetDropElement && evt.target !== targetDropElement) {
+      url = evt.target.src;
+      evt.target.src = targetDropElement.src;
+      targetDropElement.src = url;
+    }
+
+    evt.target.style.opacity = '1';
+    [].forEach.call(photoContainer.querySelectorAll('img'), function (photoElement) {
+      photoElement.style.border = PHOTO_STYLE.border;
+    });
+  }
 })();
